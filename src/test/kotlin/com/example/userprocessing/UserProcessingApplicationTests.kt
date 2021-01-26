93d1ec59acf13882f906856e269c82c8fa435324
Ilya Bogatyrev <ilia.bogatyrev@gmail.com>
2021-01-26 22:20:21 +0300

    Initial commit

---
+++ src/test/kotlin/com/example/userprocessing/UserProcessingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.userprocessing
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserProcessingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
